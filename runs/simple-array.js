cf3b84a1feb163fd725e44f9f6d67b9188b6f902
seescode <seesyong@gmail.com>
2017-02-10 11:23:46 -0500

    add comments

--- runs/simple-array.js
+++ runs/simple-array.js
@@ -1,13 +1,18 @@
-
 const normalize = require('normalizr').normalize;
 const schema = require('normalizr').schema;
 
+//Step 1: Create your schemas with an id
 const articleSchema = new schema.Entity('article', {}, { idAttribute: 'id' });
 const authorSchema = new schema.Entity('author', {}, { idAttribute: 'id' });
 
+//Step 2: Define relationships between your schemas
 articleSchema.define({
     author: authorSchema
 });
+
+//Step 3: Run normalize
+var output = normalize(data, [articleSchema]);
+
 
 var data = [{
     id: 1,
@@ -25,7 +30,6 @@ var data = [{
     }
 }];
 
-var output = normalize(data, [articleSchema]);
 console.log('Simple array run: ')
 console.log(JSON.stringify(output, null, 2));
 console.log('');
